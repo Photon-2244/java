96f51d5a98151c837eb7f82aa6cf8053b32c11cc
Abhishek Paul <www.abhishekpaul@gmail.com>
2021-12-17 15:16:51 +0530

    00.00.00.01

---
+++ FirstFrame/FirstFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+class FirstFrame
+{
+   public static void main(String [] args)
+     {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+     }
+}
---
+++ FirstFrame/FirstFrame1.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+class FirstFrame1 extends JFrame
+{
+   firstFrame ()
+      {
+        this.setVisible(true); //last statement.
+      }
+   public static void main(String [] args)
+     {
+        new FirstFrame();
+     }
+}
